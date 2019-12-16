7d59d57d4ebb949beff29e6388878dc506fb6084
yang <392539259@qq.com>
2019-12-16 10:21:54 +0800

    test request

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(bodyParser.json());
 
 app.post("/api/git/autodeploy", (req, res) => {
   console.log(req.body);
+
+  const { repository } = req.body;
+  const { html_url } = repository;
+
+  console.log("====");
+  console.log(html_url);
+  console.log("====");
   // const result = shell.exec("cd /root/html/deploy &&./dep.sh");
   const result = {
     code: 0
