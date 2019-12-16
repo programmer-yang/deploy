68a8b43fa5214f4948a1e94b87a2c01218d55974
yang <392539259@qq.com>
2019-12-16 10:51:28 +0800

    test

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.post("/api/git/autodeploy", (req, res) => {
   const projectName = html_url.substr(html_url.lastIndexOf("/") + 1);
   const result = shell.exec(`cd /root/html/deploy &&./shell/${projectName}.sh`);
 
+  console.log(`${projectName}.sh`);
+  console.log(result);
+
   if (result.code === 0) {
     res
       .status(200)
