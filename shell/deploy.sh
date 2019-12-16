0d6d4aa14240a9f82aca49f9f247b5b4418ebe81
yang <392539259@qq.com>
2019-12-16 10:43:23 +0800

    update script

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -8,7 +8,7 @@ git pull origin master
 
 echo 'post-receive: stop serve' \
 && (pm2 delete 'deploy' || true) \
-&& echo 'post-receive: start new serve'
+&& echo 'post-receive: start new serve' \
 && pm2 start index.js --name deploy \
 
 echo "end"
