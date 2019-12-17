089c0bc6f7f17e0f296f1135111b22bf13dc9996
yang <392539259@qq.com>
2019-12-17 10:51:54 +0800

    test

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post("/api/git/autodeploy", (req, res) => {
     `cd /root/html/yang/deploy/shell && ./${projectName}.sh`
   );
 
-  console.log(`开始执行： ${projectName}.sh`);
+  console.log(`开始执行：${projectName}.sh`);
 
   if (result.code === 0) {
     res
