c5d8074ea81de3fd1407f6c465b7b5ae787d4aff
yang <392539259@qq.com>
2019-12-17 11:03:07 +0800

    update

--- shell/blog.sh
+++ shell/blog.sh
@@ -10,7 +10,7 @@ echo 'post-receive: building…' \
 && npm run build \
 && echo 'post-receive: → done.' \
 && rm -rf /usr/share/nginx/yangblog \
-&& cp -rf ~/html/yangblog/public /usr/share/nginx/yangblog \
+&& cp -rf ~/html/yang/blog/public /usr/share/nginx/yangblog \
 && /usr/sbin/nginx -s reload \
 && echo 'post-receive: nginx reload success'
 
