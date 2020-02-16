e3eb06fc867abea59d01f2a9e4e94d5ffcf63d95
yang <392539259@qq.com>
2020-02-16 09:23:46 +0800

    update static-web config

--- shell/static-web.sh
+++ shell/static-web.sh
@@ -6,11 +6,16 @@ cd '/root/html/yang/static-web'
 
 git pull origin master
 
+{
 echo 'post-receive: building…' \
 && npm run build \
 && echo 'post-receive: → done.' \
-&& (pm2 delete 'static-web' || true) \
-&& pm2 serve ./dist 9011 --name static-web --spa \
-&& echo 'post-receive: app started successfully with pm2.'
+&& rm -rf /usr/share/nginx/filemanage \
+&& cp -rf ~/html/yang/blog/dist /usr/share/nginx/filemanage \
+&& /usr/sbin/nginx -s reload \
+&& echo 'post-receive: nginx reload success'
+} || {
+  echo 'error'
+}
 
 echo "end"
